1eb8ad62fe4247663ef25a0e59b2994ef1e511ce
Dhrumil Rangani <rangani@bc.edu>
2020-09-08 12:50:55 -0400

    In class founder button problem

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageFounder1: UILabel!
+    
+    @IBOutlet weak var messageFounder2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func messagebutton1(_ sender: Any) {
+        print("pressed1")
+        messageFounder1.text="Larry Page"
+        messageFounder2.text="Sergey Brin"
+    }
+    @IBAction func messagebutton2pressed(_ sender: Any) {
+        print("pressed2")
+        messageFounder1.text="Jennifer Hyman"
+        messageFounder2.text="Jenny Fleiss"
+    }
+    @IBAction func messagebutton3pressed(_ sender: Any) {
+        messageFounder1.text=""
+        messageFounder2.text=""
+    }
 }
 
